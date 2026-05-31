129508354a2cb4e0c92bc598a1d8f7a675febfcc
yt4tty-wq <yt4tty@gmail.com>
2026-06-01 01:28:10 +0700

    Update GameContext.cs
    Signed-off-by: yt4tty-wq <yt4tty@gmail.com>

--- Intersect.Server.Core/Database/GameData/GameContext.cs
+++ Intersect.Server.Core/Database/GameData/GameContext.cs
@@ -32,6 +32,8 @@ public abstract partial class GameContext : IntersectDbContext<GameContext>, IGa
 
     public DbSet<CraftingTableDescriptor> CraftingTables { get; set; }
 
+    public DbSet<AuctionListing> AuctionListings { get; set; }
+    
     //Classes
     public DbSet<ClassDescriptor> Classes { get; set; }
 
