7ced25f08f1f8bac149e82f67933bc836f28721c
yt4tty-wq <yt4tty@gmail.com>
2026-05-31 23:51:48 +0700

    Update MarketService.cs
    Signed-off-by: yt4tty-wq <yt4tty@gmail.com>

--- Intersect.Server/Core/MarketService.cs
+++ Intersect.Server/Core/MarketService.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Collections.Generic;
+
 namespace Intersect.Server.Core;
 
 public static class MarketService
 {
     public static List<MarketItem> Listings { get; } = new();
+
+    static MarketService()
+    {
+        Listings.Add(new MarketItem()
+        {
+            SellerId = Guid.Empty,
+            ItemId = Guid.Empty,
+            Quantity = 1,
+            Price = 1000
+        });
+    }
 }
