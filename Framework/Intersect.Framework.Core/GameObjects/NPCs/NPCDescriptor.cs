b36adc514fc6f9e52df228fb0b4b6b5645b9d33a
yt4tty-wq <yt4tty@gmail.com>
2026-06-03 02:34:40 +0700

    Update NPCDescriptor.cs
    Signed-off-by: yt4tty-wq <yt4tty@gmail.com>

--- Framework/Intersect.Framework.Core/GameObjects/NPCs/NPCDescriptor.cs
+++ Framework/Intersect.Framework.Core/GameObjects/NPCs/NPCDescriptor.cs
@@ -10,12 +10,15 @@ using Intersect.Utilities;
 using Newtonsoft.Json;
 
 namespace Intersect.Framework.Core.GameObjects.NPCs;
-public bool Passable { get; set; } = false;
+
 public partial class NPCDescriptor : DatabaseObject<NPCDescriptor>, IFolderable
 {
     private long[] _maxVitals = new long[Enum.GetValues<Vital>().Length];
     private int[] _stats = new int[Enum.GetValues<Stat>().Length];
     private long[] _vitalRegen = new long[Enum.GetValues<Vital>().Length];
+
+    // --- ย้ายมาอยู่ตรงนี้ (ข้างใน Class) เพื่อแก้ Error CS0116 เรียบร้อยครับ ---
+    public bool Passable { get; set; } = false;
 
     [NotMapped]
     public ConditionLists AttackOnSightConditions { get; set; } = new();
